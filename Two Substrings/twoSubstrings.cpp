30a404bd2b0db8ad6b6af9d390df5841df8805c7
anikatasnin <jarintasnin09@gmail.com>
2025-05-19 13:55:31 +0600

    added Two Substrings

--- Two Substrings/twoSubstrings.cpp
+++ Two Substrings/twoSubstrings.cpp
@@ -1,54 +1,56 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     long long int i, j, f, n, m, x, p, fnd;
     string s;
 
-    while(getline(cin,s)){
+    while (getline(cin, s)) {
         f = 0;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == 'A' && s[i + 1] == 'B'){
+        p = 0;
+
+        for (i = 0; i < s.size() - 1; i++) {
+            if (s[i] == 'A' && s[i + 1] == 'B') {
                 fnd = i;
                 p = 1;
                 break;
             }
         }
-        if(p == 1){
-            for(j = fnd + 2; j < s.size(); j++){
-                if(s[j] == 'B' && s[j + 1] == 'A'){
+
+        if (p == 1) {
+            for (j = fnd + 2; j < s.size() - 1; j++) {
+                if (s[j] == 'B' && s[j + 1] == 'A') {
                     f = 1;
                     break;
                 }
             }
         }
 
-         if(f == 0){
-            for(i = 0; i < s.size(); i++){
-                if(s[i]=='B' && s[i + 1] == 'A'){
+        if (f == 0) {
+            for (i = 0; i < s.size() - 1; i++) {
+                if (s[i] == 'B' && s[i + 1] == 'A') {
                     fnd = i;
                     p = 2;
                     break;
                 }
             }
 
-        if(p == 2){
-            for(j = fnd + 2; j < s.size(); j++){
-                if(s[j] == 'A' && s[j + 1] == 'B'){
-                    f = 1;
-                    break;
+            if (p == 2) {
+                for (j = fnd + 2; j < s.size() - 1; j++) {
+                    if (s[j] == 'A' && s[j + 1] == 'B') {
+                        f = 1;
+                        break;
+                    }
                 }
             }
         }
-    }
 
-    if(f == 1){
-        cout << "YES" << endl;
+        if (f == 1) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
-    else{
-        cout << "NO" << endl;
-    }
-
 
     return 0;
 }
