095d7c7b6c4006185386c4d8550e419d778160da
anikatasnin <jarintasnin09@gmail.com>
2025-06-20 10:47:09 +0600

    added Little Girl and Maximum Sum

--- Little Girl and Maximum Sum/littleGirlAndMaximumSum.cpp
+++ Little Girl and Maximum Sum/littleGirlAndMaximumSum.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+
+    vector<int> a(n);
+    vector<int> freq(n + 1, 0);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < q; ++i) {
+        int l, r;
+        cin >> l >> r;
+        freq[l - 1] += 1;
+        freq[r] -= 1;
+    }
+
+    for (int i = 1; i < n; ++i) {
+        freq[i] += freq[i - 1];
+    }
+
+    freq.pop_back();
+
+    sort(a.begin(), a.end());
+    sort(freq.begin(), freq.end());
+
+    long long sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += 1LL * a[i] * freq[i];
+    }
+
+    cout << sum << endl;
+    return 0;
+}
 
