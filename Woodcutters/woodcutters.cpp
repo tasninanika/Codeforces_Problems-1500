fd9265b82e073a42c46dd395d7e9b0476effc333
anikatasnin <jarintasnin09@gmail.com>
2025-06-25 09:25:14 +0600

    added Woodcutters

--- Woodcutters/woodcutters.cpp
+++ Woodcutters/woodcutters.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n], b[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i] >> b[i];
+    }
+
+    int c = 2;
+
+    if (n == 1) {
+        c = 1;
+    }
+    else{
+        for(int i = 1; i < n - 1; i++){
+            if (a[i] - a[i - 1] > b[i]) {
+                c++;
+            }
+            else if(a[i + 1] - a[i] > b[i]){
+                a[i] += b[i];
+                c++;
+            }
+        }
+    }
+
+    cout << c << endl;
+
+    return 0;
+}
 
